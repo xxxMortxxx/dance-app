6e16438b6c9bd3788e269142527ec00d50256646
Oleksandr Krupko <Oleksandr_Krupko@epam.com>
2018-11-02 19:56:35 +0200

    build folder

---
+++ docs/precache-manifest.214d5b1ff00c11a59078d46f333cfd5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a876087f69f81344501",
+    "url": "/static/css/main.f8f2a48b.chunk.css"
+  },
+  {
+    "revision": "4a876087f69f81344501",
+    "url": "/static/js/main.4a876087.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5f72ef6ff1b7c17e746f9fcf95cdfbfd",
+    "url": "/index.html"
+  }
+];
