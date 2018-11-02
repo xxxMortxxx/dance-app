ec6fa26e4b7779db2394687895618ddae8807c07
Oleksandr Krupko <Oleksandr_Krupko@epam.com>
2018-11-02 20:06:48 +0200

    setted homepage value

---
+++ docs/precache-manifest.5601dce40923f089d8f08c06dcb473d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1796704b5a80ac5a3d7c",
+    "url": "/dance-app/static/css/main.f8f2a48b.chunk.css"
+  },
+  {
+    "revision": "1796704b5a80ac5a3d7c",
+    "url": "/dance-app/static/js/main.1796704b.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/dance-app/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "8f7ff318ef84b2a4fccf",
+    "url": "/dance-app/static/js/runtime~main.8f7ff318.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dance-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b1d19eebd0ad4cd58b0558605c782ebf",
+    "url": "/dance-app/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.214d5b1ff00c11a59078d46f333cfd5c.js"
+  "/dance-app/precache-manifest.5601dce40923f089d8f08c06dcb473d6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/dance-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
